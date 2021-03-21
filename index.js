122d54890ea4426f48110bb44850e5e8ea96580b
brennankapollock <brennankapollock@gmail.com>
2021-03-21 09:01:20 -0500

    Freah

---
+++ index.js
@@ -0,0 +1,106 @@
+const submitHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const errors = validateForm(formData);
+
+    const errorElements = document.querySelectorAll(".error");
+        for (let element of errorElements) {
+            element.style.display = "none";
+    }
+
+    Object.keys(errors).forEach((key) => {
+        // find the specific error element
+        const errorElement = document.querySelector(`#park_${key} .error`);
+        errorElement.innerHTML = errors[key];
+        errorElement.style.display = "block";
+      });
+      //if no errors are detected
+      if (!Object.keys(errors).length) {
+        //create a new element
+        const parkSection = document.createElement("section");
+      
+        // add the park class
+        parkSection.classList.add("park");
+      
+        // construct the HTML for this element
+        const content = `
+          <h2>${formData.get("name")}</h2>
+          <div class="location">${formData.get("location")}</div>
+          <div class="description">${formData.get("description")}</div>
+          <button class="rateBtn" title="Add to Favourites">&#9734;</button>
+          <div class="stats">
+            <div class="established stat">
+              <h3>Established</h3>
+              <div class="value">${formData.get("established")}(
+                "MMMM D, YYYY"
+              )}</div>
+            </div>
+            <div class="area stat">
+              <h3>Area</h3>
+              <div class="value">${formData.get("area")}</div>
+            </div>
+            <div class="rating stat">
+              <h3>Rating</h3>
+              <div class="value">${formData.get("rating")}</div>
+            </div>
+          </div>
+          `;
+      
+        // set the innerHTML
+        parkSection.innerHTML = content;
+      
+        //append to the main element
+        document.querySelector("main").appendChild(parkSection);
+      }
+    
+};
+
+const main = () => {
+    const form = document.querySelector("#parkForm");
+    form.addEventListener("submit", submitHandler);
+}
+
+
+function validateExists(value) {
+    return value && value.trim();
+  }
+
+
+function validateForm(formData) {
+    const errors = {};
+  
+    // check if name was entered
+    if (!validateExists(formData.get("name"))) {
+      errors.name = "Please enter a name";
+    }
+  
+    // check if rating was entered
+    if (!validateExists(formData.get("rating"))) {
+      errors.rating = "Please enter a rating";
+    }
+  
+    // check if description was entered
+    if (!validateExists(formData.get("description"))) {
+      errors.description = "Please enter short description";
+    }
+  
+    // check if established date was entered
+    if (!validateExists(formData.get("established"))) {
+      errors.established = "Please enter date";
+    }
+  
+    // check if area was entered
+    if (!validateExists(formData.get("area"))) {
+      errors.area = "Please enter the area of the park";
+    }
+  
+    // check if location date was entered
+    if (!validateExists(formData.get("location"))) {
+      errors.location = "Please enter the location of the park";
+    }
+  
+    return errors;
+  }
+
+window.addEventListener("DOMContentLoaded", main);
